b866573695f05dcbc522f27093922df5e977d3b1
valor <valord577@gmail.com>
2018-11-22 16:04:24 +0800

    Initial commit

---
+++ src/main/java/org/elsa/filemanager/FileManagerApplication.java
@@ -0,0 +1,12 @@
+package org.elsa.filemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileManagerApplication.class, args);
+    }
+}
